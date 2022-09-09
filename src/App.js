56c7ee2b04ab1c57f8c1aafd790d43f6ecf640ec
Rajesh Yadav <ry2922000@gmail.com>
2022-09-09 14:07:32 +0530

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,58 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
+const data = [
+  {
+    question : "Question 1",
+    answer : `Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores numquam omnis, quis atque obcaecati eveniet hic quaerat, itaque nostrum soluta eaque, voluptas dolorem repudiandae ipsam cum illo. Quisquam, itaque molestias.`
+  },
+  {
+    question : "Question 2",
+    answer : `Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores numquam omnis, quis atque obcaecati eveniet hic quaerat, itaque nostrum soluta eaque, voluptas dolorem repudiandae ipsam cum illo. Quisquam, itaque molestias.`
+  },
+  {
+    question : "Question 3",
+    answer : `Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores numquam omnis, quis atque obcaecati eveniet hic quaerat, itaque nostrum soluta eaque, voluptas dolorem repudiandae ipsam cum illo. Quisquam, itaque molestias.`
+  },
+  {
+    question : "Question 4",
+    answer : `Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores numquam omnis, quis atque obcaecati eveniet hic quaerat, itaque nostrum soluta eaque, voluptas dolorem repudiandae ipsam cum illo. Quisquam, itaque molestias.`
+  },
+  {
+    question : "Question 5",
+    answer : `Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores numquam omnis, quis atque obcaecati eveniet hic quaerat, itaque nostrum soluta eaque, voluptas dolorem repudiandae ipsam cum illo. Quisquam, itaque molestias.`
+  },
+]
+
+
+
+
 function App() {
+
+  let [selected, setSeleted] = useState(null)
+
+  let toggle = (i)=> {
+    if (selected === i) {
+      return setSeleted(null)
+    }
+    setSeleted(i)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    
+    <div className="wrapper">
+      <div className="accordion">
+        {
+          data.map((item, i)=> (
+            <div className="item" key={i}>
+              <div className="title" onClick={()=> toggle(i)}>
+                <h2>{item.question}</h2> <span>{ selected  === i? "➖" : "➕"}</span>
+              </div>
+              <div className={ selected  === i? "content show":"content" }>{item.answer}</div>
+            </div>
+          ))
+        }
+      </div>
     </div>
   );
 }
